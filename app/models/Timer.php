3caced2aa937fab820c138c60cb5110218fa837f
shafie0 <shafie.abla@gmail.com>
2014-11-04 00:28:24 +0000

    Add new methods
    Add start and stop
    Use Carbon (methods) instead of date

--- app/models/Timer.php
+++ app/models/Timer.php
@@ -1,4 +1,5 @@
 <?php
+use Carbon\Carbon;
 
 class Timer extends Eloquent {
 	protected $guarded = array();
@@ -11,5 +12,41 @@ class Timer extends Eloquent {
 	{
 		return $this->belongsTo('User', 'user_id');
 		
+	}
+
+	/**
+	 * Start the timer.
+	 *
+	 * @return Response
+	 */
+	public static function start()
+	{
+		$id=Input::get('timer_id');
+		
+		if(!is_null($id))
+		{
+			$dt = Carbon::now();
+			$timer=Timer::find($id);
+			$timer->startdate=$dt->toDateTimeString();
+			$timer->save();
+		}
+	}
+
+	/**
+	 * Stop (finish) the timer.
+	 *
+	 * @return Response
+	 */
+	public static function stop()
+	{
+		$id=Input::get('timer_id');
+		
+		if(!is_null($id))
+		{
+			$dt = Carbon::now();
+			$timer=Timer::find($id);
+			$timer->enddate=$dt->toDateTimeString();
+			$timer->save();
+		}
 	}
 }
